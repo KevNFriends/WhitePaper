fab9375e2b1e0b5ad04bcaed7e14c193cf0e513b
KevNFriends <50630721+KevNFriends@users.noreply.github.com>
2022-04-17 21:55:02 -0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -11,8 +11,8 @@ export default {
   ),
   head: (
     <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#b48bd8" />
+      <meta name="msapplication-TileColor" content="#b48bd8" />
+      <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
       <meta name="description" content="KevNFriends" />
